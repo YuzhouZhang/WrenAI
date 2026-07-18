653ddc65cd19cc5b5dd2b5a95c64c3a15922b390
章羽舟 <zhangyuzhou@zj.chinamobile.com>
2026-07-18 09:49:06 +0800

    feat: add custom App component with favicon, GlobalConfig, Apollo, and PostHog providers

--- wren-ui/src/pages/_app.tsx
+++ wren-ui/src/pages/_app.tsx
@@ -16,7 +16,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Wren AI</title>
+        <title>O域数据模型</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <GlobalConfigProvider>
